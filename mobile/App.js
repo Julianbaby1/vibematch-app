0a0ff9a6410e548c73ba76253c39a29a36158546
Julianbaby1 <Julianwheeler502@yahoo.com>
2026-04-25 22:03:53 -0400

    Add React Native starter

---
+++ mobile/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>VibeMatch Mobile App Coming Soon</Text>
+    </View>
+  );
+}
